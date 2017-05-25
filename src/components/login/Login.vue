21f1eb2edd24caec17710f11b87fda6b6f40326a
Groude <thekirchik@gmail.com>
2017-05-25 21:40:51 +0300

    created login page, get fonts

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container">
+<!-- <div class="container">
   <form v-on:submit.prevent="submitLoginForm()">
     <div class="form-group row">
       <div class="col-sm-5">
@@ -17,10 +17,30 @@
       </div>
     </div>
   </form>
-</div>
+</div> -->
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="login">
+        <form class="login-form form" v-on:submit.prevent="submitLoginForm()">
+          <div class="form-title">
+            <img src="../../assets/logo.png" alt="logo" class="form-logo">
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-input" v-model="user.email" placeholder="type your email">
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-input" v-model="user.password" placeholder="type your password">
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-block">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</main>
 </template>
 
 <script src="./Login.js"></script>
-<style lang="scss" scoped src="./login.scss">
-
-</style>
+<style lang="scss" src="./login.scss"></style>
