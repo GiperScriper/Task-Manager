b7f9be690efc1c5b827750050b0629f45a6913de
Groude <thekirchik@gmail.com>
2017-06-15 10:47:07 +0300

    styled card project

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="dashboard">
-    <h1>Dashboard Component</h1>
+    <div class="card card-add">
+      <button type="button" name="button">
+        <icon name="plus-circle" class="card-plus" scale="5"></icon>
+      </button>
+    </div>
+
+    <div class="card">
+      <div class="card-header">
+        <h3>Task name</h3>
+        <icon name="check-circle-o" class="card-check" scale="1.2"></icon>
+      </div>
+      <div class="card-body">
+        <p>
+          But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system
+        </p>
+      </div>
+      <div class="card-footer">
+        <span class="card-project">Blog</span>
+      </div>
+    </div>
   </div>
 </template>
 
