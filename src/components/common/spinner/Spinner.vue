8f6f24b59d21b4be7ae3434227d4a081799436c3
Nikolay Martynov <tty0sh@gmail.com>
2017-06-24 15:22:18 +0300

    added spinner styles

--- src/components/common/spinner/Spinner.vue
+++ src/components/common/spinner/Spinner.vue
@@ -1,13 +1,50 @@
 <template>
-  <div class="spinner" v-if="isLoading">
-    <h1>Loading...</h1>
+  <div class="loader" v-if="isLoading">
   </div>
 </template>
 
 <script src="./Spinner.js"></script>
 <style lang="scss" scoped>
-  .spinner {
-    background: white;
-    z-index: 999;
+  .loader,
+  .loader:after {
+    border-radius: 50%;
+    width: 10em;
+    height: 10em;
   }
+  .loader {
+    margin: 0 auto;
+    font-size: 10px;
+    position: relative;
+    text-indent: -9999em;
+    border-top: 0.9em solid rgba(0,98,196, 0.2);
+    border-right: 0.9em solid rgba(0,98,196, 0.2);
+    border-bottom: 0.9em solid rgba(0,98,196, 0.2);
+    border-left: 0.9em solid #3695ff;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation: load8 1.1s infinite linear;
+    animation: load8 1.1s infinite linear;
+  }
+  @-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+  }
+  @keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+  }
+
 </style>
